6939836905e051a188bb69b4d0fccf2af4853508
Hines Harmon <hinesharmon2005@gmail.com>
2025-09-19 19:23:23 -0400

    Added a header and footer, We can add more items to navbar as we make them. Footer also sticks to bottom instead of floating. Need to implement SignUp part, it is just there with no link (href).

--- JobRecruiter/JobRecruiter/settings.py
+++ JobRecruiter/JobRecruiter/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+# This setting tells Django where to collect all static files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
